63ea6aea687200c016e1b6affd0eada567a8a034
Sam Burba <sburba@mail.csuchico.edu>
2019-10-15 15:41:43 -0700

    updated to simplify and add ascii graphics

--- health.go
+++ health.go
@@ -7,7 +7,11 @@ import(
 	"time"
 	"log"
 	"strconv"
+	"fmt"
+	"strings"
 )
+
+const WaitTime = 3
 
 type Transport struct {
 	rtp			http.RoundTripper
@@ -59,32 +63,66 @@ func (tr *Transport) Duration() time.Duration {
     return tr.reqEnd.Sub(tr.reqStart)
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatalln("Usage health <uri>")
-		os.Exit(0)
-	}
-	count := 1
-	if len(os.Args) > 2 && len(os.Args) < 5 && os.Args[2] == "-c" {
-		input_count, err := strconv.Atoi(os.Args[3])
-		count = input_count;
-		if err != nil {
-			log.Fatalln(err)
-			os.Exit(0)
-		}
-	}
-	url := os.Args[1]
-	tp := newTransport();
-	client := &http.Client{Transport: tp}
-	for i := 0; i < count; i++ {
-	resp, err := client.Get(url)
+func getURI(uri string, client *http.Client) *http.Response {
+	resp, err := client.Get(uri)
 	if err != nil {
 		log.Fatalln(err)
 		os.Exit(0)
+	}	
+	return resp
+}
+
+func printStandard(uri string, resp *http.Response, tp *Transport) {
+	log.Printf("Response from %s: %s", uri, resp.Status)
+	log.Println("Duration:", tp.Duration())
+	log.Println("Request duration:", tp.ReqDuration())
+	log.Println("Connection duration:", tp.ConnDuration())
+}
+
+func printCondensed(uri string, resp *http.Response, tp *Transport) {
+	fmt.Printf("\rResponse from %s: %s - Duration: %s", uri, resp.Status, tp.Duration())
+}
+func printGraphic(uri string, resp *http.Response, tp *Transport) {
+	percent := int(tp.Duration() / 10000000)
+	total := 100 - percent
+	if total > 0 {
+		fmt.Printf("\r%s%s\t%s\n", strings.Repeat("*", percent), strings.Repeat("-", total), tp.Duration())
+	} else {
+		fmt.Printf("\r%s\t%s\n", strings.Repeat("*", percent),tp.Duration())
 	}
-		log.Printf("Response from %s: %s", url, resp.Status)
-		log.Println("Duration:", tp.Duration())
-		log.Println("Request duration:", tp.ReqDuration())
-		log.Println("Connection duration:", tp.ConnDuration())
+	fmt.Printf("\rResponse from %s: %s - Duration: %s", uri, resp.Status, tp.Duration())
+}
+
+func main() {
+	tp := newTransport();
+	client := &http.Client{Transport: tp}
+	if len(os.Args) == 2 {
+		resp := getURI(os.Args[1], client)
+		printStandard(os.Args[1], resp, tp)
+	} else if len(os.Args) == 4 && os.Args[2] == "-c" {
+		count, err := strconv.Atoi(os.Args[3])
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(0)
+		}	
+		for i := 0; i < count; i++ {
+			resp := getURI(os.Args[1], client)
+			printStandard(os.Args[1], resp, tp)
+		}
+	} else if len(os.Args) == 3 && os.Args[2] == "-r" {
+		for {
+			resp := getURI(os.Args[1], client)
+			printCondensed(os.Args[1], resp, tp)
+			time.Sleep(WaitTime * time.Second)
+		}		
+	} else if len(os.Args) == 3 && os.Args[2] == "-g" {
+		for {
+			resp := getURI(os.Args[1], client)
+			printGraphic(os.Args[1], resp, tp)
+			time.Sleep(WaitTime * time.Second)
+		}		
+	} else {
+		log.Fatalln("Usage: health <uri> [-c <count> | -r | -g]")
+		os.Exit(0)
 	}
 }
